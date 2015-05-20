3153c7e9069ba3cd17366669f485a74bba4241b1
Dayne Broderson <dayne@alaska.edu>
2015-05-20 01:55:48 -0800

    mend

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'broderson@gmail.com'
 license 'MIT'
 description 'Installs/Configures n1nj4net-dns'
 long_description 'Installs/Configures n1nj4net-dns'
-version '0.1.4'
+version '0.1.5'
 
 depends 'dnsmasq', '~> 0.2.0'
