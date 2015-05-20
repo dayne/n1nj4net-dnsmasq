48f42e9fa77bde2cfef93371d98abb2cf2ca37b3
Dayne Broderson <dayne@alaska.edu>
2015-05-20 01:10:03 -0800

    fixing default gateway issue

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,14 +13,19 @@ node.override[:dnsmasq][:enable_dhcp] = true
 node.override[:dnsmasq]['dhcp'] = {
     'dhcp-authoritative' => nil,
     'dhcp-range' => 'eth0,10.47.11.100,10.47.11.200,200h',
-    'dhcp-option' => '3', #turns off everything except basic DHCP
     'domain' => 'n1nj4.net',
+    'expand-hosts' => nil,
+    'local' => '/n1nj4.net/',
     'interface' => 'eth0',
+    'log-dhcp' => nil,
     'dhcp-boot' => 'pxelinux.0',
     'enable-tftp' => nil,
     'tftp-root' => '/opt/tftpboot',
     'tftp-secure' => nil
 }
+node.override[:dnsmasq]['dhcp-options'] = [
+  'eth0,3,10.47.11.1', # default gateway
+]
 
 include_recipe 'dnsmasq::dns'
 include_recipe 'dnsmasq::dhcp'
